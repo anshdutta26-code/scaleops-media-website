b9f88031cc61e8f256c9aeca8f1913e97dac988b
anshdutta26-code <anshdutta26@gmail.com>
2026-09-05 11:35:57 +0530

    Assemble approved ScaleOps logo data

---
+++ v7-preview/logo-data.js
@@ -0,0 +1,13 @@
+import c0 from './logo/chunk0.js';
+import c1 from './logo/chunk1.js';
+import c2 from './logo/chunk2.js';
+import c3 from './logo/chunk3.js';
+import c4 from './logo/chunk4.js';
+import c5 from './logo/chunk5.js';
+import c6 from './logo/chunk6.js';
+
+export const LOGO_URI = 'data:image/png;base64,' + [c0,c1,c2,c3,c4,c5,c6].join('');
+export const LOGO_ASPECT = 896 / 819;
+export const LOGO_WIDTH = 896;
+export const LOGO_HEIGHT = 819;
+export default LOGO_URI;
